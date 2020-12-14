64c81309608c37b2bc3ab462ac64e10053b0cd3c
Jamalahmad123 <45999703+Jamalahmad123@users.noreply.github.com>
2020-12-14 22:36:59 +0500

    initial commited

---
+++ js/app.js
@@ -0,0 +1,23 @@
+'use strict';
+
+// Nav UI element
+const navHeader = document.querySelector('.nav-header');
+const sidebarList = document.querySelector('.sidebar');
+const hambugerMenu = document.querySelector('.hamburger');
+const hamnurgerLine = document.querySelectorAll('.hamburger .line');
+
+// apply fixed nav to header
+window.addEventListener('scroll', function () {
+  if (window.pageYOffset > 80) {
+    navHeader.classList.add('fixed-nav');
+  } else {
+    navHeader.classList.remove('fixed-nav');
+  }
+});
+
+// sidebar menu
+hambugerMenu.addEventListener('click', function () {
+  sidebarList.classList.toggle('show-links');
+  hambugerMenu.classList.toggle('change');
+  hambugerMenu.classList.toggle('change-color');
+});
