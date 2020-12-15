ced4244b8fe3914ac61045523aef3ca064c6aaed
Jamalahmad123 <45999703+Jamalahmad123@users.noreply.github.com>
2020-12-15 21:25:31 +0500

    changes

--- js/app.js
+++ js/app.js
@@ -4,6 +4,9 @@
 const navHeader = document.querySelector('.nav-header');
 const sidebarList = document.querySelector('.sidebar');
 const hambugerMenu = document.querySelector('.hamburger');
+// DarkMode UI
+// let darkMode = localStorage.getItem('darkMode');
+// const darkModeToggle = document.querySelector('#dark-mode-toggle');
 
 // apply fixed nav to header
 window.addEventListener('scroll', function () {
@@ -19,4 +22,13 @@ hambugerMenu.addEventListener('click', function () {
   sidebarList.classList.toggle('show-links');
   hambugerMenu.classList.toggle('change');
   hambugerMenu.classList.toggle('change-color');
+});
+
+// close sidebar menu when click on links
+sidebarList.addEventListener('click', function (event) {
+  if (event.target.classList.contains('link')) {
+    sidebarList.classList.remove('show-links');
+    hambugerMenu.classList.remove('change');
+    hambugerMenu.classList.remove('change-color');
+  }
 });
